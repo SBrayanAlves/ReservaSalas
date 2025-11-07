b56ea193a8c5435481eab73862c4fe27c6e62603
SBrayanAlves <sbrayanalves@gmail.com>
2025-11-06 22:03:21 -0300

    Irmao, pprt eu to quase desistindo

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home#, Cadastrar
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    #path('cadastrar/', Cadastrar.as_view(), name='cadastrar' )
 ]
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,11 +1,14 @@
+import string
+import re
 from django import forms
 from django.forms import ModelForm
 from usuarios.models import Usuario
-import re
 
 class ValidacaoUsuario(forms.ModelForm):
 
+
     senha = forms.CharField(widget=forms.PasswordInput(), label='senha', required=True)
+    senha2 = forms.CharField(widget=forms.PasswordInput(), label='confirmacaosenha', required=True)
     email_institucional = forms.EmailField(required=True)
     
     #cria um formulario com os campos abaixo para validacao dos dados
@@ -39,8 +42,26 @@ class ValidacaoUsuario(forms.ModelForm):
         return telefone_validado
     
     def clean_senha(self):
+        maisculas = string.ascii_uppercase
+        minusculas = string.ascii_lowercase
+        caracteres = string.punctuation
+        
         senha = self.cleaned_data.get('senha')
-        if not senha:
+        senhaconfirmada = self.cleaned_data.get('confirmacaosenha')
+
+        if len(senha) < 8:
+            raise forms.ValidationError('A senha precisa ter no minimo 8 digitos')
+        
+        if not any(char in maisculas for char in senha):
+            raise forms.ValidationError('A senha precisa ter uma letra maiscula')
+        
+        if not any(char in minusculas for char in senha):
+            raise forms.ValidationError('A senha precisa ter uma letra minuscula')
+        
+        if not any(char in caracteres for char in senha):
+            raise forms.ValidationError('A senha precisa ter um caractere')
+        
+        if senha and senhaconfirmada and senha != senhaconfirmada:
             raise forms.ValidationError('Coloque a senha')
         return senha
     
