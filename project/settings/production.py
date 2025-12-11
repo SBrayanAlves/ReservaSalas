9cc8aace1ca5a4c7c135d156c4fbefb1b79128b8
SBrayanAlves <sbrayanalves@gmail.com>
2025-12-11 15:47:28 -0300

    Preparando ambiente para deploy

---
+++ project/settings/production.py
@@ -0,0 +1,21 @@
+from .base import *
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# Database
+
+DATABASES = {
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
+
+# Configurações de Segurança Adicionais (HTTPS)
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
